582958c472e9aca99ab2a57e50b0d5182294f7b6
Tsigie <tktsigie@yahoo.com>
2018-12-19 00:43:23 -0500

    Added file

---
+++ client/d3_geo.js
@@ -0,0 +1,8 @@
+<script src="https://d3js.org/d3-array.v1.min.js"></script>
+<script src="https://d3js.org/d3-geo.v1.min.js"></script>
+<script>
+
+var projection = d3.geoEqualEarth(),
+    path = d3.geoPath(projection);
+
+</script>
